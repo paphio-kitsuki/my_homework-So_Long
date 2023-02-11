d1838c3f4aa88cb82220dc8438a8d4878bd102a3
Itsuki <IT.King5963@gmail.com>
2023-02-11 16:24:03 +0900

    save1

--- main.c
+++ main.c
@@ -1,31 +1,39 @@
-#include "mlx.h"
-#include <stdio.h>
-#include <stdlib.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kitsuki <kitsuki@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 16:09:04 by kitsuki           #+#    #+#             */
+/*   Updated: 2023/02/11 16:23:39 by kitsuki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-typedef struct s_window
+#include "so_long.h"
+
+int	exit_func(int button, t_frame *param)
 {
-	void	*mlx;
-	void	*win;
-}			t_window;
-
-
-int	exit_func(int button, t_window *param)
-{
-	printf("%d\n", button);
 	mlx_destroy_display(param->mlx);
 	mlx_destroy_window(param->mlx, param->win);
 	exit(0);
 }
 
+int	key_notify(int button, int x, int y, t_frame *param)
+{
+	printf("%d\n", button);
+}
+
 int	main()
 {
-	t_window	*window;
+	t_frame	*frame;
 
-	window = malloc(sizeof(t_window));
-	window->mlx = mlx_init();
-	window->win = mlx_new_window(window->mlx, 500, 500, "mlx 42");
-	//mlx_mouse_hook(window->win, exit_func, window);
-	mlx_hook(window->win, 17, 0, exit_func, window);
-	mlx_loop(window->mlx);
-	free(window);
+	frame = malloc(sizeof(t_frame));
+	frame->mlx = mlx_init();
+	frame->win = mlx_new_window(frame->mlx, 500, 500, "mlx 42");
+	//mlx_mouse_hook(frame->win, exit_func, frame);
+	mlx_hook(frame->win, DESTROY_NOTIFY, NOEVENT_MASK, exit_func, frame);
+	mlx_key_hook(frame->win, key_notify, frame);
+	mlx_loop(frame->mlx);
+	free(frame);
 }
---
+++ so_long.h
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kitsuki <kitsuki@student.42tokyo.jp>       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/11 16:08:23 by kitsuki           #+#    #+#             */
+/*   Updated: 2023/02/11 16:08:23 by kitsuki          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx/mlx.h"
+# include <stdio.h>
+# include <stdlib.h>
+
+# define DESTROY_NOTIFY	17
+
+# define NOEVENT_MASK	0
+
+typedef struct s_frame
+{
+	void	*mlx;
+	void	*win;
+}			t_frame;
+
+
+
+#endif
