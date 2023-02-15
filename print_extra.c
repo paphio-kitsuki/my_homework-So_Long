bf25b9b54943bc198d66b5597b005217047fe8a9
Itsuki <IT.King5963@gmail.com>
2023-02-15 15:32:43 +0900

    save-b2-2

--- print_extra.c
+++ print_extra.c
@@ -19,7 +19,7 @@ void	print_extra(t_frame *frame, char *str)
 	if (str != NULL)
 	{
 		printstr_with_scale(frame, str, STREDGECOLOR, 7);
-		printstr_with_scale(frame, str, STRCOLOR, 3);
+		printstr_with_scale(frame, str, STRCOLOR, 2);
 	}
 }
 
@@ -31,6 +31,8 @@ static void	printstr_with_scale(t_frame *f, char *str, int color, int size)
 	if (f == NULL || str == NULL || size <= 0)
 		return ;
 	i = - (size - 1) / 2;
+	if (size % 2 == 0)
+		i --;
 	while (i <= (size - 1) / 2)
 	{
 		j = - (size - 1) / 2;
